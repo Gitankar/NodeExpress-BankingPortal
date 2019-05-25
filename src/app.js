a4ac5870436d41be6deaf3af94ddc6b4ed094967
gitankar <gitankar.puggal@gmail.com>
2019-05-25 14:49:46 +0530

    fixed erro

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const accountRoutes = require('./routes/account');
+const accountRoutes = require('./routes/accounts');
 const servicesRoutes = require('./routes/services');
 
 const app = express();
